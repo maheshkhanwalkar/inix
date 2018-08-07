01e065878527ca085c70ac7a07eccded1693e20c
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-08-07 13:18:10 -0400

    Build: Switch to CMake build system
    The top-level CMakeLists.txt outlines the "high-level" build, which
    would be applicable regardless of what specific target is chosen.
    
    For each supported architecture, there will be a CMakeLists.txt, which
    will add arch-specific command line flags and specify which files need
    to be built, based on the provided option flags.
    
    The switch from custom Makefile to CMake was necessary, since the
    previous system was not robust and would not allow for the required
    level of granularity for feature inclusion/exclusion.

--- include/libk/string.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <stddef.h>
-
-void* memcpy(void* dest, const void* src, size_t num);
-
-char* strcpy(char* dest, const char* src);
-char* strncpy(char* dest, const char* src, size_t num);
-
-size_t strlen(const char* str);
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,5 @@
-#include <include/boot/entry.h>
 
-void kernel_main(struct boot_info* b_info)
+void kernel_main()
 {
-    (void)b_info;
+
 }
