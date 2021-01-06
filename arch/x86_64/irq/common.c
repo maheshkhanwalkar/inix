32896228ddacb9baebba46512f4fad14b5c34234
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2021-01-05 22:08:49 -0500

    x86_64: setup handlers for each exception ISR
    Create handlers __isrX, where X is the exception number, that are
    referenced within the IDT.
    
    These handlers *always* push the exception number onto the stack, which
    allows the particular exception to be identified from the single common
    handler function which processes all IRQs.
    
    To ensure a consistent stack state, the __isrX handlers will also push
    an error code to the stack for all exceptions that don't have an error
    code pushed by the CPU. This artificial error code is meaningless and
    hard-coded to be 0.
    
    The function _irq_common_base saves the current register state onto the
    stack and calls irq_common_handler() for actual processing. Upon return,
    it restores the register state and performs an IRETQ to the
    pre-interrupt context.

---
+++ arch/x86_64/irq/common.c
@@ -0,0 +1,7 @@
+#include <arch/x86_64/irq/regs.h>
+
+void irq_common_handler(regs_t* regs)
+{
+    // TODO
+    (void)regs;
+}
--- arch/x86_64/irq/idt.c
+++ arch/x86_64/irq/idt.c
@@ -41,7 +41,68 @@ void idt_install_handler(unsigned short pos, unsigned long address)
     idt[pos].flags = 0x8E00; /* P + DPL=00 + Interrupt Gate */
 }
 
+// ISR references (see arch/x86_64/irq/target.S)
+extern void __isr0();
+extern void __isr1();
+extern void __isr2();
+extern void __isr3();
+extern void __isr4();
+extern void __isr5();
+extern void __isr6();
+extern void __isr7();
+extern void __isr8();
+extern void __isr9();
+extern void __isr10();
+extern void __isr11();
+extern void __isr12();
+extern void __isr13();
+extern void __isr14();
+extern void __isr15();
+extern void __isr16();
+extern void __isr17();
+extern void __isr18();
+extern void __isr19();
+extern void __isr20();
+extern void __isr21();
+extern void __isrReserved();
+
 void irq_arch_setup()
 {
+    // ISR 0-21 stub entries
+    idt_install_handler(0, (unsigned long)&__isr0);
+    idt_install_handler(1, (unsigned long)&__isr1);
+    idt_install_handler(2, (unsigned long)&__isr2);
+    idt_install_handler(3, (unsigned long)&__isr3);
+    idt_install_handler(4, (unsigned long)&__isr4);
+    idt_install_handler(5, (unsigned long)&__isr5);
+    idt_install_handler(6, (unsigned long)&__isr6);
+    idt_install_handler(7, (unsigned long)&__isr7);
+    idt_install_handler(8, (unsigned long)&__isr8);
+    idt_install_handler(9, (unsigned long)&__isr9);
+    idt_install_handler(10, (unsigned long)&__isr10);
+    idt_install_handler(11, (unsigned long)&__isr11);
+    idt_install_handler(12, (unsigned long)&__isr12);
+    idt_install_handler(13, (unsigned long)&__isr13);
+    idt_install_handler(14, (unsigned long)&__isr14);
+    idt_install_handler(15, (unsigned long)&__isr15);
+    idt_install_handler(16, (unsigned long)&__isr16);
+    idt_install_handler(17, (unsigned long)&__isr17);
+    idt_install_handler(18, (unsigned long)&__isr18);
+    idt_install_handler(19, (unsigned long)&__isr19);
+    idt_install_handler(20, (unsigned long)&__isr20);
+    idt_install_handler(21, (unsigned long)&__isr21);
+
+    // ISR 22-31 are reserved -- just use one common entry
+    idt_install_handler(22, (unsigned long)&__isrReserved);
+    idt_install_handler(23, (unsigned long)&__isrReserved);
+    idt_install_handler(24, (unsigned long)&__isrReserved);
+    idt_install_handler(25, (unsigned long)&__isrReserved);
+    idt_install_handler(26, (unsigned long)&__isrReserved);
+    idt_install_handler(27, (unsigned long)&__isrReserved);
+    idt_install_handler(28, (unsigned long)&__isrReserved);
+    idt_install_handler(29, (unsigned long)&__isrReserved);
+    idt_install_handler(30, (unsigned long)&__isrReserved);
+    idt_install_handler(31, (unsigned long)&__isrReserved);
+
     idt_install();
 }
